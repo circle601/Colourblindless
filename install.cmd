cb6fe1b053dadd7a410da275697520e153a60561
circle601@gmail.com <da325@sussex.ac.uk>
2019-11-09 15:11:38 +0000

    Intitall commit

---
+++ install.cmd
@@ -0,0 +1,5 @@
+pause
+C:\hackathon\node-v13.1.0-win-x64\npm install -g express-generator
+pause
+express
+pause
---
+++ start.cmd
@@ -0,0 +1,1 @@
+C:\hackathon\node-v13.1.0-win-x64\npm --scripts-prepend-node-path=true start 
